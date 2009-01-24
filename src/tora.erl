9c3ada659d70693b0b234bc072d9fb9961c75e98
Harish Mallipeddi <harish.mallipeddi@gmail.com>
2009-01-24 20:32:21 +0800

    Finished all the put methods

--- src/tora.erl
+++ src/tora.erl
@@ -21,9 +21,23 @@
 -define(TT_DEFAULT_HOST, "localhost").
 -define(TT_DEFAULT_PORT, 1978).
 
+-define(KEYSIZE, apply(fun () -> KeySize = byte_size(Key), <<KeySize:32>> end, [])).
+-define(VALSIZE, apply(fun () -> ValSize = byte_size(Value), <<ValSize:32>> end, [])).
+
+%% Tokyo Tyrant protocol - Command IDs
+-define(CID_PUT, <<16#c810:16>>).
+-define(CID_PUTKEEP, <<16#c811:16>>).
+-define(CID_PUTCAT, <<16#c812:16>>).
+-define(CID_PUTSH1, <<16#c813:16>>).
+-define(CID_PUTNR, <<16#c818:16>>).
+
+-define(CID_GET, <<16#c830:16>>).
+
 %% API
 -export([
-    connect/0, connect/2, put/2, get/1
+    connect/0, connect/2, 
+    put/2, putkeep/2, putcat/2, putsh1/3, putnr/2,
+    get/1
 ]).
 
 %% gen_server callbacks
@@ -46,9 +60,19 @@ connect() ->
     connect(?TT_DEFAULT_HOST, ?TT_DEFAULT_PORT).
 connect(Host, Port) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [Host, Port], []).
-put(Key, Value) ->
+
+put(Key, Value) when is_list(Key) andalso is_binary(Value) ->
     gen_server:call(?SERVER, {put, {list_to_binary(Key), Value}}).
-get(Key) ->
+putkeep(Key, Value) when is_list(Key) andalso is_binary(Value) ->
+    gen_server:call(?SERVER, {putkeep, {list_to_binary(Key), Value}}).
+putcat(Key, Value) when is_list(Key) andalso is_binary(Value) ->
+    gen_server:call(?SERVER, {putcat, {list_to_binary(Key), Value}}).
+putsh1(Key, Value, Width) when is_list(Key) andalso is_binary(Value) andalso is_integer(Width) ->
+    gen_server:call(?SERVER, {putsh1, {list_to_binary(Key), Value, Width}}).    
+putnr(Key, Value) when is_list(Key) andalso is_binary(Value) ->
+    gen_server:cast(?SERVER, {putnr, {list_to_binary(Key), Value}}).
+
+get(Key) when is_list(Key) ->
     gen_server:call(?SERVER, {get, {list_to_binary(Key)}}).
 
 %%====================================================================
@@ -59,29 +83,36 @@ init([Host, Port]) ->
     {ok, #state{socket = Sock}}.
 
 handle_call({put, {Key, Value}}, _From, #state{socket=Sock}) ->
-    KeySize = byte_size(Key),
-    ValSize = byte_size(Value),
-    gen_tcp:send(Sock, iolist_to_binary([<<16#c810:16, KeySize:32, ValSize:32>>, Key, Value])),
-    Reply = receive
-        {tcp, _, <<0:8>>} -> ok;
-        {tcp, _, <<Code:8>>} -> {error, Code};
-        {error, closed} -> connection_closed
-    after ?TIMEOUT ->
-        timeout
-    end,
+    gen_tcp:send(Sock, iolist_to_binary([?CID_PUT, ?KEYSIZE, ?VALSIZE, Key, Value])),
+    Reply = recv_reply(),
     {reply, Reply, #state{socket=Sock}};
+
+handle_call({putkeep, {Key, Value}}, _From, #state{socket=Sock}) ->
+    gen_tcp:send(Sock, iolist_to_binary([?CID_PUTKEEP, ?KEYSIZE, ?VALSIZE, Key, Value])),
+    Reply = recv_reply(),
+    {reply, Reply, #state{socket=Sock}};
+
+handle_call({putcat, {Key, Value}}, _From, #state{socket=Sock}) ->
+    gen_tcp:send(Sock, iolist_to_binary([?CID_PUTCAT, ?KEYSIZE, ?VALSIZE, Key, Value])),
+    Reply = recv_reply(),
+    {reply, Reply, #state{socket=Sock}};
+
+handle_call({putsh1, {Key, Value, Width}}, _From, #state{socket=Sock}) ->
+    gen_tcp:send(Sock, iolist_to_binary([?CID_PUTSH1, ?KEYSIZE, ?VALSIZE, <<Width:32>>, Key, Value])),
+    Reply = recv_reply(),
+    {reply, Reply, #state{socket=Sock}};
+
 handle_call({get, {Key}}, _From, #state{socket=Sock}) ->
-    KeySize = byte_size(Key),
-    gen_tcp:send(Sock, iolist_to_binary([<<16#c830:16, KeySize:32>>, Key])),
-    Reply = receive
-        {tcp, _, <<0:8, _ValSize:32, Value/binary>>} -> Value;
-        {tcp, _, <<Code:8, _/binary>>} -> {error, Code};
-        {error, closed} -> connection_closed
-    after ?TIMEOUT ->
-        timeout
+    gen_tcp:send(Sock, iolist_to_binary([?CID_GET, ?KEYSIZE, Key])),
+    Reply = case recv_reply() of
+        {ok, <<_ValSize:32, Value/binary>>} -> Value;
+        R -> R
     end,
     {reply, Reply, #state{socket=Sock}}.
 
+handle_cast({putnr, {Key, Value}}, #state{socket=Sock}) ->
+    gen_tcp:send(Sock, iolist_to_binary([?CID_PUTNR, ?KEYSIZE, ?VALSIZE, Key, Value])),
+    {noreply, #state{socket=Sock}};
 handle_cast(_Msg, State) -> {noreply, State}.
 
 handle_info(_Info, State) -> {noreply, State}.
@@ -95,6 +126,13 @@ terminate(_Reason, #state{socket=Sock}) ->
 %%====================================================================
 %% Private stuff
 %%====================================================================
-
-
+recv_reply() ->
+    receive
+        {tcp, _, <<0:8>>} -> ok;
+        {tcp, _, <<0:8, Rest/binary>>} -> {ok, Rest};
+        {tcp, _, <<Code:8, _/binary>>} -> {error, Code};
+        {error, closed} -> connection_closed
+    after ?TIMEOUT ->
+        timeout
+    end.
 
--- test/tora_test.erl
+++ test/tora_test.erl
@@ -2,11 +2,38 @@ setup() ->
     tora:connect().
 
 put_get() ->
-    ok = tora:put("key1", <<"harish">>),
-    ok = tora:put("key2", <<2#1101:32>>),
-    <<"harish">> = tora:get("key1"),
-    <<2#1101:32>> = tora:get("key2").
+    ok = tora:put("put_get1", <<"harish">>),
+    ok = tora:put("put_get2", <<2#1101:32>>),
+    <<"harish">> = tora:get("put_get1"),
+    <<2#1101:32>> = tora:get("put_get2").
+
+putkeep() ->
+    ok = tora:put("putkeep1", <<"apple">>),
+    {error, _} = tora:putkeep("putkeep1", <<"orange">>),
+    <<"apple">> = tora:get("putkeep1"), % no effect if key already exists before putkeep
+    
+    ok = tora:putkeep("putkeep2", <<"orange">>),
+    <<"orange">> = tora:get("putkeep2"). % puts the key if key does not exist already
+
+putcat() ->
+    ok = tora:put("putcat1", <<"foo">>),
+    % append "bar" to the end
+    ok = tora:putcat("putcat1", <<"bar">>),
+    <<"foobar">> = tora:get("putcat1").
+
+putsh1() ->
+    ok = tora:put("putsh1", <<"foo">>),
+    % append "bar" to the end and shift to the lift to retain the width of "4"
+    ok = tora:putsh1("putsh1", <<"bar">>, 4),
+    <<"obar">> = tora:get("putsh1").
+
+putnr() ->
+    tora:putnr("putnr1", <<"mango">>),
+    <<"mango">> = tora:get("putnr1").
+
+put_tests() ->
+    [fun put_get/0, fun putkeep/0, fun putcat/0, fun putsh1/0, fun putnr/0].
 
 gen_test_() ->
-    Tests = [fun put_get/0],
+    Tests = [put_tests()],
     {inorder, {setup, fun setup/0, Tests}}.
